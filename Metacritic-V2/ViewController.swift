6a82b3233971e44cd34dbbfc0ef2d27d96d9e0ce
Ajay Raj <s12768262@CC1308T2-DM2701.local>
2015-11-12 09:18:37 +0000

    Initial Commit

---
+++ Metacritic-V2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Metacritic-V2
+//
+//  Created by Ajay Raj on 12/11/2015.
+//  Copyright (c) 2015 Ajay Raj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Metacritic-V2Tests/Metacritic_V2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Metacritic_V2Tests.swift
+//  Metacritic-V2Tests
+//
+//  Created by Ajay Raj on 12/11/2015.
+//  Copyright (c) 2015 Ajay Raj. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Metacritic_V2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
